15b27e91680f0f3b4607a29b41da6df96e8f2711
Renne Castrucci <rennecastrucci@gmail.com>
2023-05-10 15:28:20 -0300

    Tool documentation

--- src/main/java/com/insane/eyewalk/api/utils/Tool.java
+++ src/main/java/com/insane/eyewalk/api/utils/Tool.java
@@ -25,12 +25,23 @@ public class Tool {
         return getFileExtension(Objects.requireNonNull(file.getOriginalFilename()));
     }
 
+    /**
+     * Method to check if a folder exists. If it doesn't exist the method will try to create it
+     * @param folderName the folder name to be checked or created
+     * @return boolean true if exists or if it was created successfully
+     */
     public static boolean checkCreateFolder(String folderName) {
         File folder = new File(folderName);
         if (!folder.exists()) return folder.mkdirs();
         else return true;
     }
 
+    /**
+     * Method to concatenate a filename with its extension
+     * @param filename a file name
+     * @param extension a file extension
+     * @return a String with the filename and extension concatenated with a period.
+     */
     public static String concatFileExtension(String filename, String extension) {
         return filename+"."+extension;
     }
